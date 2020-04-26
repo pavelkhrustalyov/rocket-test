8c6a17a400fe6b8debf6043df35113c2acf5724c
Pavel Khrustalyov <pavel_khrustalyov@mail.ru>
2020-04-26 14:27:35 +0300

    first commit

---
+++ docs/precache-manifest.7a4985903c50754c1e3666ed16a6a6ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323181ba9390230cfa100dc727f5d868",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86e429e1156da42f0e00",
+    "url": "/static/css/main.f3d56cae.chunk.css"
+  },
+  {
+    "revision": "6fa06556b735a77d8de1",
+    "url": "/static/js/2.bcf9cf62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bcf9cf62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86e429e1156da42f0e00",
+    "url": "/static/js/main.928f8dbe.chunk.js"
+  },
+  {
+    "revision": "88f5ace7990953b1af51",
+    "url": "/static/js/runtime-main.01452d68.js"
+  }
+]);
